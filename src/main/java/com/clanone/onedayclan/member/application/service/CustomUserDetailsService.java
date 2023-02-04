1252ea9f27d10624f8a7649b30121cfac5161043
JiONi <uphp179@gmail.com>
2023-02-04 20:18:19 +0900

    feat: 로그인 실패 시 에러 핸들링 추가

--- src/main/java/com/clanone/onedayclan/member/application/service/CustomUserDetailsService.java
+++ src/main/java/com/clanone/onedayclan/member/application/service/CustomUserDetailsService.java
@@ -18,10 +18,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String id) {
         return getMemberPort.getMemberById(id)
                 .map(this::createUserDetails)
-                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+                .orElseThrow(() -> new UsernameNotFoundException("회원정보가 일치하지 않습니다.\n" + "다시 확인해 주세요."));
     }
 
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
