10b0bc52d869862ea62b51ee65b729ac34241758
jeewonn <jeewonn0504@gmail.com>
2023-03-11 23:00:37 +0900

    fix: inquiry seq 추가

--- src/main/java/com/clanone/onedayclan/customer/adapter/in/web/response/InquiryListResponse.java
+++ src/main/java/com/clanone/onedayclan/customer/adapter/in/web/response/InquiryListResponse.java
@@ -9,12 +9,14 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class InquiryListResponse {
+    private long seq;
     private String title;
     private boolean answerYn;
     private LocalDateTime createdAt;
 
     public static InquiryListResponse of(InquiryEntity inquiry){
         return InquiryListResponse.builder()
+                .seq(inquiry.getSeq())
                 .title(inquiry.getTitle())
                 .answerYn(inquiry.isAnswerYn())
                 .createdAt(inquiry.getCreatedAt())
