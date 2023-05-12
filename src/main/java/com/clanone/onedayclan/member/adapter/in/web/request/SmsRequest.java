b2b5a8f4f9808e07391a04a2c85d2323d6b532f4
JiONi <uphp179@gmail.com>
2023-05-12 21:23:44 +0900

    feat: 문자 발송 시 '-' 까지 입력받도록 수정

--- src/main/java/com/clanone/onedayclan/member/adapter/in/web/request/SmsRequest.java
+++ src/main/java/com/clanone/onedayclan/member/adapter/in/web/request/SmsRequest.java
@@ -7,4 +7,8 @@ import lombok.Getter;
 public class SmsRequest {
     @NotBlank(message = "받을 사람의 휴대폰 번호를 입력해 주세요.")
     private String to;
+
+    public String getTo() {
+        return to.replaceAll("-","");
+    }
 }
